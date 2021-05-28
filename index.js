b50f31df1ff5999d0e49d08679e7e5770f95e7c3
Cooper-Faram <cbfaram@gmail.com>
2021-05-28 11:30:06 +1000

    Adds home route

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const PORT = process.env.PORT || 3000
 
 const data = require('./data')
 
+app.get('/', (req, res) => {
+    res.send("Welcome to our schedule website")
+})
+
 app.listen(3000, () => console.log(`App listening at http://localhost:${PORT}`))
